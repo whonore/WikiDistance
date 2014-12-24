3b362c65a5fedc36dad2e3799bf9849608cbe523
whonore <whonore@u.rochester.edu>
2014-12-24 15:28:49 -0500

    Can get first link in the main page content

--- wikiDist.py
+++ wikiDist.py
@@ -15,4 +15,20 @@ def getTitle(source):
     titleRe = re.compile(".*<title>(.*) - Wikipedia, the free encyclopedia</title>.*")
     return titleRe.match(source).group(1)
 
-print(getTitle(fetchPage()))
+def getFirstLink(source):
+    linkRe = re.compile(".*<a href=\"/wiki/(.+)\" title.*")
+    contentStart = source.find("mw-content-text")
+    pStart = source[contentStart:-1].find("<p>") + contentStart
+    aStart = source[pStart:-1].find("<a href=\"/wiki/") + pStart
+    aEnd = source[aStart:-1].find("/a>") + aStart
+    try:
+        return linkRe.match(source[pStart:aEnd]).group(1)
+    except AttributeError:
+        sys.exit("Nope bad")
+    
+page = fetchPage()
+title = getTitle(page)
+while title != "Philosophy":
+    title = getTitle(page)
+    print(title)
+    page = fetchPage(getFirstLink(page))
